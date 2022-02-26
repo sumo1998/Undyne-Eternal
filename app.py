23e46746a0bcc0cecae6bdb2391ad7696c4511c6
SumukhNitundila <sumukh14@gmail.com>
2022-02-26 16:26:01 -0600

    Loading app secret from env

--- app.py
+++ app.py
@@ -2,15 +2,20 @@
 # Removed the structure of "client" module and just added a common templates folder
 # Made it like this to make the structure conform to what flask expects. Just a little less manual path specification
 # But if the project grows bigger, we can change the structure to be more modular
+import os
 
+from dotenv import load_dotenv, find_dotenv
 from flask import Flask
-from factory import object_factory
-
-app = Flask(__name__)
-app.secret_key = '21344'
-object_factory._create_auth_object(app)
 
 from api.auth import auth_router
+from factory import object_factory
+
+load_dotenv(find_dotenv())
+
+app = Flask(__name__)
+
+app.secret_key = os.getenv('APP_SECRET')
+object_factory._create_auth_object(app)
 
 app.register_blueprint(auth_router.auth_blueprint)
 
