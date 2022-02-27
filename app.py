75fc1470f4514656d50985c9f18e256fe3384a4a
siyadgedi <gedisiyad@gmail.com>
2022-02-27 16:39:29 -0600

    Added files

--- app.py
+++ app.py
@@ -3,11 +3,12 @@
 # Made it like this to make the structure conform to what flask expects. Just a little less manual path specification
 # But if the project grows bigger, we can change the structure to be more modular
 
-from flask import Flask
+from flask import Flask, render_template
 from factory import object_factory
 
 app = Flask(__name__)
 app.secret_key='21344'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 object_factory.get_auth_object(app)
 from api.auth import auth_router
 
@@ -16,7 +17,7 @@ app.register_blueprint(auth_router.auth_blueprint)
 
 @app.route("/")
 def home_page():
-    return "<h1>This is the homepage for the app</h1>"
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
