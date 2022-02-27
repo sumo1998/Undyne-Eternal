c35f6d7c17b5099022b5d76f9d1f3d03a6e992ee
Sumanth <vsk21098@gmail.com>
2022-02-27 00:15:24 -0600

    Added apis and sql files for user page

---
+++ db/user/user_handler.py
@@ -0,0 +1,24 @@
+from flask import render_template_string
+from db import database_handler
+
+def getUserInfo(id):
+    query = ""
+    with open("./db/user/user_info.sql") as f:
+        query = f.read()
+        query = render_template_string(query,user_id=id)
+
+        with database_handler.get_db_cursor() as cur:
+            cur.execute(query)
+            res = cur.fetchall()
+            return res
+
+def getUserLevels(id):
+    query = ""
+    with open("./db/user/user_levels.sql") as f:
+        query = f.read()
+        query = render_template_string(query,user_id=id)
+
+        with database_handler.get_db_cursor() as cur:
+            cur.execute(query)
+            res = cur.fetchall()
+            return res
---
+++ db/user/user_levels.sql
@@ -0,0 +1,1 @@
+select * from levels where user_id = '{{user_id}}' order by level_rating desc;
