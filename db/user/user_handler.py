9002969c94dfe1224c94ab245173e137e1ffde6d
Sumanth <vsk21098@gmail.com>
2022-03-07 21:51:16 -0600

    Added update api for user

--- db/user/user_handler.py
+++ db/user/user_handler.py
@@ -30,3 +30,25 @@ def getUserLevels(id):
             cur.execute(query,id)
             res = cur.fetchall()
             return res
+
+def updateUser(data):
+    uid = ""
+    user_name = ""
+    user_avatar = ""
+
+    for key in data:
+        if key == "userId":
+            uid = int(data[key])
+        elif key == "userName":
+            user_name = data[key]
+        elif key == "userAvatar":
+            user_avatar = data[key]
+
+    query = ""
+    with open('./db/user/sql/updateUser.sql') as f:
+        query = f.read()
+        
+        with database_handler.get_db_cursor(True) as cur:
+            cur.execute(query,(user_name,user_avatar,uid))
+            print("Executed query")
+    
