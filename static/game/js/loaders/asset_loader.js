71acaf38c4d279996d72f8e5f5b3e77214d1962c
Nikunj Chawla <nikchawla312@gmail.com>
2022-03-14 04:11:23 -0500

    Added initial AssetLoader class and FontLoader class

---
+++ static/game/js/loaders/asset_loader.js
@@ -0,0 +1,14 @@
+/**
+ * Represents an asset to be loaded.
+ */
+class AssetLoader {
+    /**
+     * Creates an AssetLoader instance.
+     */
+    constructor() {}
+    
+    /**
+     * True if the asset has been loaded.
+     */
+    isLoaded() {}
+}
--- static/game/js/loaders/font_loader.js
+++ static/game/js/loaders/font_loader.js
@@ -1,3 +1,52 @@
-class FontLoader {
+/**
+ * Loads the fonts for the game.
+ */
+class FontLoader extends AssetLoader {
+    /**
+     * Responsible for the loading of the fonts.
+     */
+    #fontLoader;
     
+    /**
+     * True when the fonts are loaded.
+     */
+    #loaded;
+    
+    /**
+     * Loads the provided fonts for PIXI
+     * @param fontNameDirectoryObject The names of the fonts to load and their corresponding directories
+     */
+    constructor(fontNameDirectoryObject) {
+        super();
+        this.#loaded = false;
+        this.#fontLoader = new PIXI.loaders.Loader();
+        
+        for(const fontName in fontNameDirectoryObject) {
+            this.#fontLoader.add(fontName, fontNameDirectoryObject[fontName]);
+        }
+        
+        this.#fontLoader.on("complete", (loader, resources) => this.#setUpFonts(resources));
+    }
+    
+    /**
+     * Sets the fonts up when loaded to use the NEAREST scale mode for Pixellated scaling.
+     * @param resources The resources loaded by the font loader
+     */
+    #setUpFonts(resources) {
+        for(const resource in resources) {
+            if(resource.endsWith("_image")) {
+                resources[resource].texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
+            }
+        }
+        
+        this.#loaded = true;
+    }
+    
+    /**
+     * Returns true if the fonts are loaded.
+     * @returns True if the fonts are loaded
+     */
+    isLoaded() {
+        return this.#loaded;
+    }
 }
