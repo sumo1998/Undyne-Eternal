52910da642e74f2aae83ca2df457575c6a25525b
Nikunj Chawla <nikchawla312@gmail.com>
2022-03-13 21:20:09 -0500

    Modified resolution to use twice the device-pixel ratio at minimum to prevent quality loss

--- static/game/js/rendering.js
+++ static/game/js/rendering.js
@@ -4,7 +4,7 @@ function setUpRendering() {
     
     const gameWidth = 640, gameHeight = 480;
     renderer = PIXI.autoDetectRenderer(gameWidth, gameHeight,
-        {transparent: true, resolution: 2, antialias: true, autoDensity: true}
+        {transparent: true, resolution: Math.max(window.devicePixelRatio * 2, 2), antialias: true, autoDensity: true}
     );
     
     gameplayStage = new PIXI.Container();
