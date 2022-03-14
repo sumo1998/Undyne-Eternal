b3c6a4c163ff3d82a6a0bfa52aa6da1606a42272
Nikunj Chawla <nikchawla312@gmail.com>
2022-03-13 20:58:57 -0500

    Modified permitted arrows to those allowed by the level editor

--- static/game/js/game/arrow.js
+++ static/game/js/game/arrow.js
@@ -98,46 +98,17 @@ function addArrowGroup(arrowGroup) {
     
     var offsetTime = attackQueue[0].time;
     
-    var randDir = Math.floor(Math.random() * 4);
-    
-    var lastDirection = 0;
-    
     for(var a = 0; a < arrowGroup.arrows.length; ++a) {
         
         var ar = arrowGroup.arrows[a];
         var direction = ar.direction;
         
-        if(direction == "R") { // random
+        if(direction === "?") { // random
             direction = Math.floor(Math.random() * 4);
         }
-        else if(direction == "D") { // random that's _not_ the next one
-            var non = lastDirection;
-            do {
-                direction = Math.floor(Math.random() * 4);
-            } while(direction == non);
+        else {
+            direction = parseInt(direction);
         }
-        else if(direction[0] == "+") { // relative to last one
-            var diff = parseInt(direction[1]);
-            direction = (lastDirection + 3 + diff) % 4;
-        }
-        else if(direction[0] == "-") { // relative to last one
-            var diff = 4 - parseInt(direction[1]);
-            direction = (lastDirection + 3 + diff) % 4;
-        }
-        else if(direction[0] == "$") { // fixed; isn't affected by global random
-            direction = parseInt(direction[1]);
-        }
-        else if(direction[0] == "N") { // random that's _not_ the specified one
-            var non = parseInt(direction[1]);
-            do {
-                direction = Math.floor(Math.random() * 4);
-            } while(direction == non);
-        }
-        else if(typeof direction === "string") {
-            direction = (randDir + 3 + parseInt(direction)) % 4;
-        }
-        
-        lastDirection = direction;
         
         var time = offsetTime + ar.targetTime;
         
