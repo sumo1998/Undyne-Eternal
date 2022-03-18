3461cd389b2a14ceadb012d300412b566ddf0691
Nikunj Chawla <nikchawla312@gmail.com>
2022-03-18 18:13:59 -0500

    Added back to home feed button in game

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 # Made it like this to make the structure conform to what flask expects. Just a little less manual path specification
 # But if the project grows bigger, we can change the structure to be more modular
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 from api.auth import auth_router
 from factory import object_factory
@@ -17,6 +17,11 @@ app.register_blueprint(auth_router.auth_blueprint)
 
 @app.route("/")
 def home_page():
+    return redirect(url_for("feed"))
+
+
+@app.route("/home-feed")
+def feed():
     return "<h1>This is the homepage for the app</h1><a href='game'>Click here</a>"
 
 
