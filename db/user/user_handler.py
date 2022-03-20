3e6cad986b8df415c0d19d105a1a0f0d6e066bde
Sumanth <vsk21098@gmail.com>
2022-03-20 12:48:16 -0500

    refactored user_handler.py

--- db/user/user_handler.py
+++ db/user/user_handler.py
@@ -4,7 +4,6 @@ from db.user.user_pydantic import UpdateUser
 
 
 def get_user_info(id):
-    
     with open("./db/user/sql/user_info.sql") as f:
         query = f.read()
         
@@ -15,7 +14,6 @@ def get_user_info(id):
 
 
 def get_user_levels(id):
-    
     with open("./db/user/sql/user_levels.sql") as f:
         query = f.read()
         
@@ -31,7 +29,6 @@ def update_user(data):
     uid = d['userId']
     user_name = d['userName']
     user_avatar = d['userAvatar']
-    
     
     with open('./db/user/sql/updateUser.sql') as f:
         query = f.read()
