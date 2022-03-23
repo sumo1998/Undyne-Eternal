5222fdbc04cdd21a923de94845bbd4a7d2e39aed
SumukhNitundila <sumukh14@gmail.com>
2022-03-23 02:12:58 -0500

    Fixed user info

--- db/user/sql/user_info.sql
+++ db/user/sql/user_info.sql
@@ -1,4 +1,4 @@
-SELECT u.user_id, u.user_name, u.user_email, u.user_avatar, AVG(l.level_rating)
+SELECT u.user_id, u.user_name, u.user_email, u.user_avatar, COALESCE(AVG(l.level_rating), 0)
 FROM user_info u, levels l
 WHERE u.user_name = %s
   AND u.user_id = l.user_id
